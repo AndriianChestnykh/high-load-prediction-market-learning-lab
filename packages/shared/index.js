2c684f3914f01bf3368f5177b0b1db8e89c62fbe
Andriian Chestnykh <andriian.chestnykh@gmail.com>
2026-06-01 20:17:05 +0200

    Remove all, create new task

--- packages/shared/index.js
+++
@@ -1,4 +0,0 @@
-export { config } from './config.js';
-export { getPool, query, closePool, poolStats } from './db.js';
-export { getRedis, closeRedis } from './redis.js';
-export { registry, httpRequestDuration, metricsPlugin, startMetricsServer } from './metrics.js';
